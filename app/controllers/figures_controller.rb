41ee4fbfd3603d3f2c0fa000bdaacf22dbe9ee52
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-12-13 16:50:17 -0500

    Figures controller View form linked

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,13 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+
+    erb :'/figures/new'
+  end
+
+
+  get '/figures' do
+    @figures =Figure.all
+    erb :'/figures/index'
+  end
 end
